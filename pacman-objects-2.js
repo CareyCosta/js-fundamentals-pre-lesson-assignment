c2a92dcacbc7092475c46880229b03d63aaf7edc
Carey Costa <careyxcosta@gmail.com>
2017-02-13 14:45:37 -0500

    pacman-objects-2, var ghosts defined, values retrieved

---
+++ pacman-objects-2.js
@@ -0,0 +1,6 @@
+var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange'};
+console.log(ghosts);
+
+console.log(ghosts.inky);
+//above and below are the same
+console.log(ghosts['inky']);
