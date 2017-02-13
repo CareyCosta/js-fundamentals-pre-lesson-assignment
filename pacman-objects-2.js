87e295a8cd5bd0cc19b2b42cc0cbdaeca80f4ced
Carey Costa <careyxcosta@gmail.com>
2017-02-13 14:46:48 -0500

    notes added to code for clarity; differentiate between dot notation and square-bracket notation

--- pacman-objects-2.js
+++ pacman-objects-2.js
@@ -1,6 +1,8 @@
 var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange'};
 console.log(ghosts);
 
+//retrieve using dot notation
 console.log(ghosts.inky);
-//above and below are the same
+
+//retrieve using squre-bracket notation. note: above and below are the same
 console.log(ghosts['inky']);
